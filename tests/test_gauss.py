b77864981077148758d7dcb2c8ba6c1fb9723c8c
Pauline Thiele <Fenstersteuerungber SupportPauline.Thiele@Student.HTW-Berlin.de>
2023-01-03 17:21:03 +0100

    test value error

--- tests/test_gauss.py
+++ tests/test_gauss.py
@@ -11,9 +11,36 @@ class TestGauss(unittest.TestCase):
         ]
 
         expected = [
-            [1, 1, 0],
+            [1, 0, 0],
             [4, 1, 0],
             [7, 8, 1]
         ]
-        gauss = Gauss(matrix)
-        self.assertEqual(expected, gauss.lr_decomposition(matrix))
+        gauss = Gauss()
+        self.assertEqual(expected, gauss.lu_decomposition(matrix)[0])
+
+    def test_should_return_upper_matrix(self):
+        matrix = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
+
+        expected = [
+            [1, 2, 3],
+            [0, 1, 2],
+            [0, 0, 1]
+        ]
+        gauss = Gauss()
+        self.assertEqual(expected, gauss.lu_decomposition(matrix)[1])    
+
+    def test_should_raise_value_error(self):
+        matrix = [
+            [1, 2, 3],
+            [4, 5, 6]
+        ]
+
+        expected = ValueError
+
+        gauss = Gauss()
+        with self.assertRaises(expected): 
+            gauss.lu_decomposition(matrix)
